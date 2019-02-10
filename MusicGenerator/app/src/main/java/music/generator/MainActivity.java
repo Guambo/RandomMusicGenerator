aa737500a57359fc5545649d61ac214795f71a09
Guambo <nvallejos1234@gmail.com>
2019-02-10 06:55:36 -0500

    Finally fixed tick error, final commit for hackathon

--- MusicGenerator/app/src/main/java/music/generator/MainActivity.java
+++ MusicGenerator/app/src/main/java/music/generator/MainActivity.java
@@ -18,6 +18,7 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.app.DownloadManager;
 import android.content.Intent;
+import android.content.Context;
 
 import jp.kshoji.javax.sound.midi.*;
 import java.io.File;
@@ -51,14 +52,14 @@ public class MainActivity extends AppCompatActivity {
     // User input values: Key, Time Signature, Checkbox for producing 1. Melody 2. Bass
     // 3. Include seventh chords XOR Include major third 4. Include minor fourth 5. Include long chord progression
     public void GUI() {
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        //Spinner spinner = (Spinner) findViewById(R.id.spinner);
         // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.keys, android.R.layout.simple_spinner_item);
+        //ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+        //        R.array.keys, android.R.layout.simple_spinner_item);
         // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
+        //spinner.setAdapter(adapter);
 
         mButton = findViewById(R.id.button);
         mButton.setOnClickListener(new View.OnClickListener() {
@@ -101,13 +102,15 @@ public class MainActivity extends AppCompatActivity {
             sequencer.setTempoInBPM(bpm);
 
             MidiSystem.write(sequence, 1, midiFile);
+            DownloadManager dm = (DownloadManager)getSystemService(Context.DOWNLOAD_SERVICE);
+            dm.addCompletedDownload(filename, "sample description", false, "audio/midi", midiFile.getPath(), midiFile.length(), true);
             startActivity(new Intent(DownloadManager.ACTION_VIEW_DOWNLOADS));
         } catch(Exception e) {
             e.printStackTrace();
         }
     }
 
-    public MidiEvent makeEvent(int command, int channel, int note, int velocity, int tick) {
+    public MidiEvent makeEvent(int command, int channel, int note, int velocity, long tick) {
         MidiEvent event = null;
 
         try {
@@ -121,11 +124,50 @@ public class MainActivity extends AppCompatActivity {
         return event;
     }
 
-    // TODO: @David make very long chord progressions
+    // TODO: @David make very long chord progression
     public void generate_music() {
-        int barR = ((int) Math.random() * 8) + 2;
-        int numOfChords = (int)(Math.random()*100);
-        int key = (int) Math.random()*11;
+        int k = 0;
+        if(key.equals("Random")){
+            k = (int) (Math.random()*11);
+        }
+        else if(key.equals("C")){
+            k = 0;
+        }
+        else if(key.equals("C#")){
+            k = 1;
+        }
+        else if(key.equals("D")){
+            k = 2;
+        }
+        else if(key.equals("Eb")){
+            k = 3;
+        }
+        else if(key.equals("E")){
+            k = 4;
+        }
+        else if(key.equals("F")){
+            k = 5;
+        }
+        else if(key.equals("F#")){
+            k = 6;
+        }
+        else if(key.equals("G")){
+            k = 7;
+        }
+        else if(key.equals("G#")){
+            k = 8;
+        }
+        else if(key.equals("A")){
+            k = 9;
+        }
+        else if(key.equals("Bb")){
+            k = 10;
+        }
+        else if(key.equals("B")){
+            k = 11;
+        }
+        int barR = ((int) (Math.random() * 8)) + 3;
+        int numOfChords = (int)(Math.random()*20)+6;
         int tempo = (int) Math.random()*60 + 100;
         int upperBound = 5;
         int lowerBound = 5;
@@ -144,6 +186,7 @@ public class MainActivity extends AppCompatActivity {
                 {0, 4, 7, 11},
                 {2, 5, 9, 0},
                 {4, 7, 11, 2},
+                //{4, 8, 11},
                 {5, 9, 0, 4},
                 {7, 11, 2, 6},
                 {9, 0, 4, 7}
@@ -153,21 +196,33 @@ public class MainActivity extends AppCompatActivity {
         {
             chordProg[i] = seventh[(int) (Math.random()*seventh.length)];
         }
-        int [] bassRhythm = rhythm(barR, 40);
-        int [] cRhythm = rhythm(barR, 50);
-        int [] offRhythm = rhythm(barR, 10);
+        int [] bassRhythm = rhythm(barR, 20);
+        int [] cRhythm = rhythm(barR, 60);
+        int [] offRhythm = rhythm(barR, 15);
         int [] bassNotes;
+        int [] trebNotes;
+        int [] offNotes;
         for (int i = 0; i < numOfChords; i ++) {
 
             // note on
-            bassNotes = generateNotes(lowerBound-2, upperBound-1, barR, key, chordProg[i] , (int) (Math.random()*3));
+            bassNotes = generateNotes(lowerBound-3, upperBound-3, barR, k, chordProg[i] , (int) (Math.random()*3));
+            trebNotes = generateNotes(lowerBound-2, upperBound-2, barR, k, chordProg[i] , (int) (Math.random()*3));
+            offNotes = generateNotes(lowerBound-3, upperBound-2, barR, k, chordProg[i] , (int) (Math.random()*3));
             for(int j = 0; j<barR;j++){
                 if(bassRhythm[j]==1)
                 {
-                    track.add(makeEvent(144, 1, bassNotes[j], 100, i * barR * 4 + j * 4));
+                    track.add(makeEvent(144, 1, bassNotes[j], 100, 4*i*barR + 4*j));
+                    //track.add(makeEvent(128, 1, bassNotes[j], 100, 4*i*barR + 4*j + 5));
                 }
-                else{
-                    track.add(makeEvent(128, 1, 100, 100, i * barR * 4 + j * 4));
+                if(cRhythm[j]==1)
+                {
+                    track.add(makeEvent(144, 1, trebNotes[j], 100, 4*i*barR + 4*j));
+                    //track.add(makeEvent(128, 1, bassNotes[j], 100, 4*i*barR + 4*j + 5));
+                }
+                if(offRhythm[j]==1)
+                {
+                    track.add(makeEvent(144, 1, offNotes[j], 100, (4*i*barR + 4*j)+2));
+                    //track.add(makeEvent(128, 1, bassNotes[j], 100, 4*i*barR + 4*j + 5));
                 }
                 //track.add(makeEvent(144, 1, i, 100, i));
                 //track.add(makeEvent(144, 1, i, 100, i));
@@ -176,14 +231,18 @@ public class MainActivity extends AppCompatActivity {
             // note off
             //track.add(makeEvent(128, 1, i, 100, i + 2));
         }
-
+        /*track.add(makeEvent(144, 1, 60, 100, 15));
+        track.add(makeEvent(144, 1, 66, 100, 20));
+        track.add(makeEvent(144, 1, 67, 100, 30));
+        track.add(makeEvent(144, 1, 101, 100, 300));
+        track.add(makeEvent(128, 1, 101, 100, 305));*/
     }
 
     int[] generateNotes(int lowerBound, int upperBound, int totalNotes, int key, int[] chord, int degree) {
 
         int[] ret = new int[totalNotes];
         for(int i = 0; i < totalNotes; i++) {
-            ret[i] = chord[degree] + 12*(rand.nextInt(upperBound) + lowerBound);
+            ret[i] = (chord[degree] + 12*(rand.nextInt(upperBound) + lowerBound))+key;
         }
         return ret;
     }
@@ -194,9 +253,11 @@ public class MainActivity extends AppCompatActivity {
         for(int i = 0; i < rLength; i++) {
             if ((int)(Math.random()* 100) < percentage) {
                 retval[i] = 1;
+                Log.d("YEPPPPPPPP", "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWW");
             }
             else{
                 retval[i] = 0;
+                Log.d("NOPEE", "YEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEET");
             }
         }
         return retval;
